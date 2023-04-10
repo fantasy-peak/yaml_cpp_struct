ea592b021ffa1caec01c3808c6f5550c6c825c9a
fantasy-peak <1356346239@qq.com>
2023-04-10 11:44:06 +0900

    supports cpp11

--- example/main.cpp
+++ example/main.cpp
@@ -1,161 +1,47 @@
-// #define NOT_USE_YCS_INIT_VALUE
+#include <iostream>
 
 #include <spdlog/spdlog.h>
-#include <iostream>
 #include <yaml_cpp_struct.hpp>
 
-template <typename... Ts>
-std::string to_string(const Ts&... ts) {
-	std::stringstream ss;
-	const char* sep = "";
-	auto func = [](auto& t) {
-		if constexpr (std::is_same_v<std::decay_t<decltype(t)>, int8_t> || std::is_same_v<std::decay_t<decltype(t)>, uint8_t>)
-			return static_cast<int32_t>(t);
-		else
-			return t;
-	};
-	((static_cast<void>(ss << sep << func(ts)), sep = " "), ...);
-	return ss.str();
-}
-
-template <typename... Args>
-std::string to_string(const std::tuple<Args...>& t) {
-	return std::apply([](const auto&... ts) { return to_string(ts...); }, t);
-}
-
-template <typename T>
-inline std::enable_if_t<std::is_arithmetic<T>::value && !std::is_same<T, bool>::value, std::string> to_string(T value) {
-	return std::to_string(value);
-}
-
-inline std::string to_string(bool value) {
-	return value ? "true" : "false";
-}
-
-inline std::string to_string(const std::string& value) {
-	return value;
-}
-
-template <typename T>
-inline std::string to_string(const std::vector<T>& vector) {
-	std::string str = "[";
-	auto it = vector.begin();
-	if (it != vector.end()) {
-		str += to_string(*it);
-		++it;
-	}
-	for (; it != vector.end(); ++it) {
-		str += ",";
-		str += to_string(*it);
-	}
-	str += "]";
-	return str;
-}
-
-template <typename T>
-inline std::string to_string(const std::unordered_set<T>& vector) {
-	std::string str = "[";
-	auto it = vector.begin();
-	if (it != vector.end()) {
-		str += to_string(*it);
-		++it;
-	}
-	for (; it != vector.end(); ++it) {
-		str += ",";
-		str += to_string(*it);
-	}
-	str += "]";
-	return str;
-}
-
-template <typename K, typename V>
-inline std::string to_string(const std::unordered_map<K, V>& map) {
-	std::string str = "{";
-	auto it = map.begin();
-	if (it != map.end()) {
-		str += to_string(it->first);
-		str += "->";
-		str += to_string(it->second);
-		++it;
-	}
-	for (; it != map.end(); ++it) {
-		str += ",";
-		str += to_string(it->first);
-		str += "->";
-		str += to_string(it->second);
-	}
-	str += "}";
-	return str;
-}
-
-template <typename T>
-inline std::string to_string(const std::optional<T>& opt) {
-	std::stringstream ss;
-	if (opt) {
-		if constexpr (std::is_fundamental_v<T> || std::is_same_v<T, std::string>)
-			ss << opt.value();
-		else
-			ss << to_string(opt.value());
-	}
-	else
-		ss << "(nullopt)";
-	return ss.str();
-}
-
-enum class AccountType : uint8_t {
+enum class AccountType : uint16_t {
 	Personal = 1,
 	Company = 2,
 };
-YCS_ADD_ENUM(AccountType, Personal, Company)
-
-std::string to_string(const AccountType& type) {
-	if (type == AccountType::Personal)
-		return "Personal";
-	if (type == AccountType::Company)
+YCS_ADD_ENUM(
+	AccountType,
+	[](std::string name) {
+		if (name == "Personal")
+			return AccountType::Personal;
+		return AccountType::Company;
+	},
+	[](AccountType type) {
+		if (type == AccountType::Personal)
+			return "Personal";
 		return "Company";
-	throw std::bad_cast();
-}
+	})
 
 struct AccountInfo {
 	bool flag;
 	std::string name;
 	std::unordered_map<std::string, int> address;
-	std::optional<std::string> num;
+	tl::optional<std::string> num{tl::nullopt};
 	std::chrono::milliseconds msec;
 	std::tuple<std::string, uint8_t> tuple;
 	std::unordered_map<std::string, std::tuple<std::string, int32_t>> map_tuple;
 };
 YCS_ADD_STRUCT(AccountInfo, flag, name, address, num, msec, tuple, map_tuple)
 
-std::string to_string(const AccountInfo& aif) {
-	std::stringstream ss;
-	ss << "AccountInfo"
-	   << " name=" << aif.name << " address=" << to_string(aif.address) << " num=" << to_string(aif.num)
-	   << " msec=" << aif.msec.count() << " tuple=" << to_string(aif.tuple) << " map_tuple=" << to_string(aif.map_tuple)
-	   << " flag=" << to_string(aif.flag);
-	return ss.str();
-}
-
 struct DefaultTest {
 	bool flag{false};
 	std::string name{"hello world"};
 	std::unordered_map<std::string, int> address{{"127.0.0.1", 8856}};
-	std::optional<std::string> num{"90"};
+	tl::optional<std::string> num{"90"};
 	std::chrono::milliseconds msec{56};
 	std::tuple<std::string, uint8_t> tuple{std::make_tuple("tuple", 58)};
 	std::unordered_map<std::string, std::tuple<std::string, int32_t>> map_tuple{{"test-default", std::make_tuple("001", 23)}};
 	AccountType account_type{AccountType::Company};
 };
 YCS_ADD_STRUCT(DefaultTest, flag, name, address, num, msec, tuple, map_tuple, account_type)
-
-std::string to_string(const DefaultTest& aif) {
-	std::stringstream ss;
-	ss << "DefaultTest"
-	   << " name=" << aif.name << " address=" << to_string(aif.address) << " num=" << to_string(aif.num)
-	   << " msec=" << aif.msec.count() << " tuple=" << to_string(aif.tuple) << " map_tuple=" << to_string(aif.map_tuple)
-	   << " flag=" << to_string(aif.flag);
-	return ss.str();
-}
 
 struct Config {
 	char ch;
@@ -166,33 +52,22 @@ struct Config {
 	AccountInfo account_info;
 	std::vector<std::string> vec;
 	std::unordered_set<uint8_t> set_vec;
-	AccountType account_type;
-	std::variant<int, std::vector<int>> v1;
 	DefaultTest default_test;
 	std::string default_str{"hello default"};
-	std::optional<std::string> default_opt{std::nullopt};
+	tl::optional<std::string> default_opt{};
+	AccountType account_type{AccountType::Company};
 };
-YCS_ADD_STRUCT(Config, ch, price, count, content, map, account_info, vec, set_vec, account_type, v1, default_test, default_str, default_opt)
-
-std::string to_string(const Config& cfg) {
-	std::stringstream ss;
-	ss << "Config"
-	   << " price=" << std::to_string(cfg.price) << " count=" << (int32_t)cfg.count
-	   << " content=" << cfg.content << " map=" << to_string(cfg.map) << " account_info=" << to_string(cfg.account_info)
-	   << " vec=" << to_string(cfg.vec) << " set_vec=" << to_string(cfg.set_vec) << " account_type=" << to_string(cfg.account_type)
-	   << " ch=" << to_string(cfg.ch) << " v1=" << to_string(std::get<1>(cfg.v1)) << " default_test=" << to_string(cfg.default_test)
-	   << " default_str=" << to_string(cfg.default_str) << " default_opt=" << to_string(cfg.default_opt);
-	return ss.str();
-}
+YCS_ADD_STRUCT(Config, ch, price, count, content, map, account_info, vec, set_vec, default_test, default_str, default_opt, account_type)
 
 int main(int, char** argv) {
-	auto [cfg, error] = yaml_cpp_struct::from_yaml<Config>(argv[1]);
+	auto value = yaml_cpp_struct::from_yaml<Config>(argv[1]);
+	auto& cfg = std::get<0>(value);
+	auto& error = std::get<1>(value);
 	if (!cfg) {
 		spdlog::error("{}", error);
 		return -1;
 	}
-	spdlog::info("\n{}", to_string(cfg.value()));
-	auto [str, e] = yaml_cpp_struct::to_yaml(cfg.value());
-	spdlog::info("\n{}", str.value());
+	auto ret = yaml_cpp_struct::to_yaml(cfg.value());
+	spdlog::info("\n{}", std::get<0>(ret).value());
 	return 0;
 }
--- include/yaml_cpp_struct.hpp
+++ include/yaml_cpp_struct.hpp
@@ -2,15 +2,13 @@
 #define _YAML_CPP_STRUCT_HPP_
 
 #include <chrono>
-#include <optional>
 #include <tuple>
 #include <unordered_map>
 #include <unordered_set>
-#include <variant>
 
 #include <yaml-cpp/yaml.h>
-#include <magic_enum.hpp>
 #include <visit_struct/visit_struct.hpp>
+#include <tl/optional.hpp>
 
 namespace yaml_cpp_struct {
 
@@ -25,60 +23,82 @@ inline std::string string_format(const std::string& format, Args... args) {
 	return std::string(buf.get(), buf.get() + size - 1);
 }
 
-template <typename T, typename Enable = void>
-struct is_optional : std::false_type {};
-
-template <typename T>
-struct is_optional<std::optional<T>> : std::true_type {};
-
 template <typename T>
 inline T node_as(const YAML::Node& node) {
-	if constexpr (std::is_same_v<std::remove_reference_t<T>, uint8_t>)
-		return static_cast<uint8_t>(node.as<uint32_t>());
-	else
-		return node.as<T>();
+	return node.as<T>();
 }
 
 template <typename T>
-inline auto cast(const T& v) {
-	if constexpr (std::is_same_v<T, uint8_t>)
-		return static_cast<uint32_t>(v);
-	else
-		return v;
+inline auto cast(const T& v) -> T {
+	return v;
 }
 
+inline auto cast(uint8_t v) -> uint32_t {
+	return static_cast<uint32_t>(v);
+}
+
+template <typename T, T... Ints>
+struct integer_sequence {
+	typedef T value_type;
+	static constexpr std::size_t size() { return sizeof...(Ints); }
+};
+
+template <std::size_t... Ints>
+using index_sequence = integer_sequence<std::size_t, Ints...>;
+
+template <typename T, std::size_t N, T... Is>
+struct make_integer_sequence : make_integer_sequence<T, N - 1, N - 1, Is...> {};
+
+template <typename T, T... Is>
+struct make_integer_sequence<T, 0, Is...> : integer_sequence<T, Is...> {};
+
+template <std::size_t N>
+using make_index_sequence = make_integer_sequence<std::size_t, N>;
+
+template <typename... T>
+using index_sequence_for = make_index_sequence<sizeof...(T)>;
+
 template <typename... T, std::size_t... I>
-inline std::tuple<T...> yaml_node_to_tuple(const YAML::Node& node, std::index_sequence<I...>) {
+inline std::tuple<T...> yaml_node_to_tuple(const YAML::Node& node, index_sequence<I...>) {
 	return std::make_tuple(yaml_cpp_struct::node_as<T>(node[I])...);
 }
 
+template <typename N>
+void process(N&&) {}
+
+template <typename N, class T, class... Args>
+void process(N&& node, T head, Args... rest) {
+	node.push_back(yaml_cpp_struct::cast(head));
+	process(node, rest...);
+}
+
 template <typename... T, std::size_t... I>
-inline YAML::Node tuple_to_yaml_node(const std::tuple<T...>& tup, std::index_sequence<I...>) {
+inline YAML::Node tuple_to_yaml_node(const std::tuple<T...>& tup, index_sequence<I...>) {
 	YAML::Node node(YAML::NodeType::Sequence);
-	(node.push_back(yaml_cpp_struct::cast(std::get<I>(tup))), ...);
+	process(node, std::get<I>(tup)...);
 	return node;
 }
 
 template <typename T>
-inline std::tuple<std::optional<std::string>, std::string> to_yaml(const T& obj) {
+inline std::tuple<tl::optional<std::string>, std::string> to_yaml(const T& obj) {
 	try {
 		YAML::Emitter emitter;
 		emitter << YAML::Node{yaml_cpp_struct::cast(obj)};
-		return std::make_tuple(emitter.c_str(), "");
+		return std::make_tuple(std::string{emitter.c_str()}, std::string{""});
 	} catch (std::exception& e) {
-		return std::make_tuple(std::nullopt, string_format("Emitter to string: %s", e.what()));
+		return std::make_tuple(tl::nullopt, string_format("Emitter to string: %s", e.what()));
 	}
 }
 
 template <typename T>
-inline std::tuple<std::optional<T>, std::string> from_yaml(const std::string& filename) {
+inline std::tuple<tl::optional<T>, std::string> from_yaml(const std::string& filename) {
 	try {
 		const auto node = YAML::LoadFile(filename);
 		return std::make_tuple(yaml_cpp_struct::node_as<T>(node), "");
 	} catch (const YAML::BadFile& e) {
-		return std::make_tuple(std::nullopt, string_format("%s not found or broken", filename.c_str()));
+		return std::make_tuple(tl::nullopt, string_format("%s not found or broken", filename.c_str()));
 	} catch (const std::exception& e) {
-		return std::make_tuple(std::nullopt, string_format("on parsing %s: %s", filename.c_str(), e.what()));
+		return std::make_tuple(tl::nullopt, string_format("on parsing %s: %s", filename.c_str(), e.what()));
 	}
 }
 
@@ -99,8 +119,8 @@ struct convert<std::unordered_map<K, V>> {
 
 	static Node encode(const std::unordered_map<K, V>& rhs) {
 		Node node(NodeType::Map);
-		for (auto& [k, v] : rhs)
-			node.force_insert(yaml_cpp_struct::cast(k), yaml_cpp_struct::cast(v));
+		for (auto& value : rhs)
+			node.force_insert(yaml_cpp_struct::cast(value.first), yaml_cpp_struct::cast(value.second));
 		return node;
 	}
 };
@@ -139,16 +159,17 @@ struct convert<std::unordered_set<V>> {
 };
 
 template <typename T>
-struct convert<std::optional<T>> {
-	static bool decode(const Node& node, std::optional<T>& rhs) {
-		if (node.IsNull())
-			rhs = std::nullopt;
+struct convert<tl::optional<T>> {
+	static bool decode(const Node& node, tl::optional<T>& rhs) {
+		if (node.IsNull()) {
+			rhs = tl::nullopt;
+		}
 		else
 			rhs = yaml_cpp_struct::node_as<T>(node);
 		return true;
 	}
 
-	static Node encode(const std::optional<T>& rhs) {
+	static Node encode(const tl::optional<T>& rhs) {
 		if (rhs.has_value())
 			return Node(yaml_cpp_struct::cast(rhs.value()));
 		else
@@ -161,117 +182,72 @@ struct convert<std::tuple<T...>> {
 	static bool decode(const Node& node, std::tuple<T...>& rhs) {
 		if (!node.IsSequence() || (node.size() != sizeof...(T)))
 			return false;
-		rhs = yaml_cpp_struct::yaml_node_to_tuple<T...>(node, std::index_sequence_for<T...>{});
+		rhs = yaml_cpp_struct::yaml_node_to_tuple<T...>(node, yaml_cpp_struct::index_sequence_for<T...>{});
 		return true;
 	}
 
 	static Node encode(const std::tuple<T...>& rhs) {
-		return yaml_cpp_struct::tuple_to_yaml_node<T...>(rhs, std::index_sequence_for<T...>{});
-	}
-};
-
-template <typename... T>
-struct convert<std::variant<T...>> {
-	static bool decode(const Node& node, std::variant<T...>& rhs) {
-		bool flag = false;
-		([&]() {
-			if (flag)
-				return;
-			try {
-				rhs = yaml_cpp_struct::node_as<T>(node);
-				flag = true;
-			} catch (...) {
-			}
-		}(),
-			...);
-		return flag;
-	}
-
-	static Node encode(const std::variant<T...>& rhs) {
-		Node node;
-		std::visit([&](auto& v) { node = Node{yaml_cpp_struct::cast(v)}; }, rhs);
-		return node;
+		return yaml_cpp_struct::tuple_to_yaml_node<T...>(rhs, yaml_cpp_struct::index_sequence_for<T...>{});
 	}
 };
 
 } // namespace YAML
 
-#ifdef NOT_USE_YCS_INIT_VALUE
-#define YCS_ADD_STRUCT(T, ...)                                                         \
-	VISITABLE_STRUCT(T, __VA_ARGS__);                                                  \
-	namespace YAML {                                                                   \
-	template <>                                                                        \
-	struct convert<T> {                                                                \
-		static bool decode(const Node& node, T& rhs) {                                 \
-			visit_struct::for_each(rhs, [&](const char* name, auto& value) {           \
-				using ToType = std::remove_reference_t<std::decay_t<decltype(value)>>; \
-				if constexpr (yaml_cpp_struct::is_optional<ToType>()) {                \
-					try {                                                              \
-						value = yaml_cpp_struct::node_as<ToType>(node[name]);          \
-					} catch (const std::runtime_error&) {                              \
-					}                                                                  \
-				}                                                                      \
-				else                                                                   \
-					value = yaml_cpp_struct::node_as<ToType>(node[name]);              \
-			});                                                                        \
-			return true;                                                               \
-		}                                                                              \
-		static Node encode(const T& rhs) {                                             \
-			Node node(NodeType::Map);                                                  \
-			visit_struct::for_each(rhs, [&](const char* name, auto& value) {           \
-				node[name] = value;                                                    \
-			});                                                                        \
-			return node;                                                               \
-		}                                                                              \
-	};                                                                                 \
-	}
-#else
-#define YCS_ADD_STRUCT(T, ...)                                                         \
-	VISITABLE_STRUCT(T, __VA_ARGS__);                                                  \
-	namespace YAML {                                                                   \
-	template <>                                                                        \
-	struct convert<T> {                                                                \
-		static bool decode(const Node& node, T& rhs) {                                 \
-			visit_struct::for_each(rhs, [&](const char* name, auto& value) {           \
-				using ToType = std::remove_reference_t<std::decay_t<decltype(value)>>; \
-				try {                                                                  \
-					value = yaml_cpp_struct::node_as<ToType>(node[name]);              \
-				} catch (const std::runtime_error&) {                                  \
-				}                                                                      \
-			});                                                                        \
-			return true;                                                               \
-		}                                                                              \
-		static Node encode(const T& rhs) {                                             \
-			Node node(NodeType::Map);                                                  \
-			visit_struct::for_each(rhs, [&](const char* name, auto& value) {           \
-				node[name] = value;                                                    \
-			});                                                                        \
-			return node;                                                               \
-		}                                                                              \
-	};                                                                                 \
-	}
-#endif
+namespace yaml_cpp_struct {
 
-#define YCS_ADD_ENUM(E, ...)                                                                         \
-	namespace YAML {                                                                                 \
-	template <>                                                                                      \
-	struct convert<E> {                                                                              \
-		static bool decode(const Node& node, E& rhs) {                                               \
-			const auto enum_str = node.as<std::string>();                                            \
-			auto value = magic_enum::enum_cast<E>(enum_str);                                         \
-			if (value.has_value())                                                                   \
-				rhs = value.value();                                                                 \
-			else {                                                                                   \
-				auto error = yaml_cpp_struct::string_format("bad enum value: %s", enum_str.c_str()); \
-				throw YAML::RepresentationException(node.Mark(), error);                             \
-			}                                                                                        \
-			return true;                                                                             \
-		}                                                                                            \
-		static Node encode(const E& rhs) {                                                           \
-			auto enum_name = magic_enum::enum_name(rhs);                                             \
-			return Node{enum_name.data()};                                                           \
-		}                                                                                            \
-	};                                                                                               \
+template <typename N>
+struct Cpp11EncodeHelper {
+	template <typename T>
+	void operator()(const char* name, const T& value) {
+		node[name] = value;
+	}
+	N& node;
+};
+
+template <typename N>
+struct Cpp11DecodeHelper {
+	template <typename T>
+	void operator()(const char* name, T& value) {
+		try {
+			value = yaml_cpp_struct::node_as<T>(node[name]);
+		} catch (const std::runtime_error&) {
+		}
+	}
+	const N& node;
+};
+
+} // namespace yaml_cpp_struct
+
+#define YCS_ADD_STRUCT(T, ...)                                                           \
+	VISITABLE_STRUCT(T, __VA_ARGS__);                                                    \
+	namespace YAML {                                                                     \
+	template <>                                                                          \
+	struct convert<T> {                                                                  \
+		static bool decode(const Node& node, T& rhs) {                                   \
+			visit_struct::for_each(rhs, yaml_cpp_struct::Cpp11DecodeHelper<Node>{node}); \
+			return true;                                                                 \
+		}                                                                                \
+		static Node encode(const T& rhs) {                                               \
+			Node node(NodeType::Map);                                                    \
+			visit_struct::for_each(rhs, yaml_cpp_struct::Cpp11EncodeHelper<Node>{node}); \
+			return node;                                                                 \
+		}                                                                                \
+	};                                                                                   \
+	}
+
+#define YCS_ADD_ENUM(E, decode_func, encode_func)      \
+	namespace YAML {                                   \
+	template <>                                        \
+	struct convert<E> {                                \
+		static bool decode(const Node& node, E& rhs) { \
+			auto enum_str = node.as<std::string>();    \
+			rhs = decode_func(enum_str);               \
+			return true;                               \
+		}                                              \
+		static Node encode(const E& rhs) {             \
+			return Node{encode_func(rhs)};             \
+		}                                              \
+	};                                                 \
 	}
 
 #endif
